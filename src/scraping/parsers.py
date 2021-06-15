248d92ebb4199b6262018edd0c0c155014420209
sfkl-i <safa1320@gmail.com>
2021-06-15 21:47:18 +0300

    stable?

--- src/scraping/parsers.py
+++ src/scraping/parsers.py
@@ -131,9 +131,9 @@ def dou(url, city=None, language=None):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-    try:
 
+    try:
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
         driver.get(url=url)
         time.sleep(2)
         try:
